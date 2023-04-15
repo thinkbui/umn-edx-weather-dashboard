ef22953575ede8b3459c2bd5a67be0cc5b7d7348
Ian Bui <thinkbui@gmail.com>
2023-04-15 14:16:38 -0500

    Condensed fetching code a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var apiKey = "f8c008ebc30e5059d679aa6a57e4b627";
+var qParam = "Minneapolis";
 
-function getCityForecast() {
-  var qParam = "Minneapolis";
+var final_data;
+
+function getCityForecast(queryParam) {
   var url = new URL("http://api.openweathermap.org/geo/1.0/direct")
-  url.searchParams.set("q", qParam);
+  url.searchParams.set("q", queryParam);
   url.searchParams.set("appid", apiKey);
   
   fetch(url.href)
@@ -39,24 +41,33 @@ function processForecast(raw_data) {
   results = {};
   for(i=0;i<raw_data.length;i++){
     var data_date = dayjs.unix(raw_data[i]["dt"]).format("MM/DD/YYYY");
+    var raw_temp = raw_data[i]["main"]["temp"];
+    var raw_wind = raw_data[i]["wind"]["speed"];
+    var raw_humidity = raw_data[i]["main"]["humidity"];
+
     if (results[data_date]) {
-      if ( results[data_date]["temp"] < raw_data[i]["main"]["temp"] ) {
-        results[data_date]["temp"] = raw_data[i]["main"]["temp"];
-      }
-      if ( results[data_date]["wind"] < raw_data[i]["wind"]["speed"] ){
-        results[data_date]["wind"] = raw_data[i]["wind"]["speed"];
-      }
-      if ( results[data_date]["humidity"] < raw_data[i]["main"]["humidity"] ){
-        results[data_date]["humidity"] = raw_data[i]["main"]["humidity"];
-      }
+      var prev_temp = results[data_date]["temp"];
+      var prev_wind = results[data_date]["wind"];
+      var prev_humidity = results[data_date]["humidity"];
+
+      results[data_date]["temp"] = prev_temp < raw_temp ? raw_temp : prev_temp;
+      // if ( prev_temp < raw_temp ) {
+        // results[data_date]["temp"] = raw_temp;
+      // }
+      results[data_date]["wind"] = prev_wind < raw_wind ? raw_wind : prev_wind;
+      // if ( prev_wind < raw_wind ){
+        // results[data_date]["wind"] = raw_wind;
+      // }
+      results[data_date]["humidity"] = prev_humidity < raw_humidity ? raw_humidity : prev_humidity;
+      // if ( prev_humidity < raw_humidity ){
+        // results[data_date]["humidity"] = raw_humidity;
+      // }
     } else {
-      results[data_date] = {};
-      results[data_date]["temp"] = raw_data[i]["main"]["temp"];
-      results[data_date]["wind"] = raw_data[i]["wind"]["speed"];
-      results[data_date]["humidity"] = raw_data[i]["main"]["humidity"];
+      results[data_date] = {"temp":raw_temp, "wind":raw_wind, "humidity": raw_humidity};
     }
   }
   console.log(results);
+  final_data = results;
 }
 
-getCityForecast();
+getCityForecast(qParam);
